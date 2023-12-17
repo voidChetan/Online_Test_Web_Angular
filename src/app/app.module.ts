b1a4fb382e81271387f1e8089832a570869dcac1
Aratikolekar <kolekararati29@gmail.com>
2023-12-17 07:02:45 -0500

    loginpagechanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NewQuestionComponent } from './pages/new-question/new-question.component';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+
+
 import { LayoutComponent } from './pages/layout/layout.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NewQuestionComponent,
-    LayoutComponent
+    LayoutComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule
-<<<<<<< HEAD
-=======
 
->>>>>>> 7b58a72e77cb04d30f2af6f5bfe8506f4955ec0b
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
