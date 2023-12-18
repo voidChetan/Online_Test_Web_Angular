2d7879bea8981de601ffd7078f970e430fe81a35
Savitagore <savitag096@gmail.com>
2023-12-18 11:17:52 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { LayoutComponent } from './pages/layout/layout.component';
 import { UserComponent } from './pages/user/user.component';
+import { NewTestComponent } from './pages/new-test/new-test.component';
+import { NewQuestionComponent } from './pages/new-question/new-question.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 const routes: Routes = [
   {
@@ -22,6 +25,18 @@ const routes: Routes = [
         path: 'user',
         component: UserComponent,
       },
+      {
+        path:'new-test',
+        component:NewTestComponent
+      },
+      {
+        path:'new-question',
+        component:NewQuestionComponent
+      },
+      {
+        path:'category',
+        component:CategoriesComponent
+      }
     ],
   },
 ];
